85e48da5235367304dfc1f571d3e766059a71af2
Kriangkrai Prommaithong <d8kriangkrai@gmail.com>
2016-11-13 20:04:52 +0700

    add body-parser

--- index.js
+++ index.js
@@ -6,48 +6,72 @@ firebase.initializeApp({
     authDomain: "fun-fun-53400.firebaseapp.com",
     databaseURL: "https://fun-fun-53400.firebaseio.com",
     storageBucket: "fun-fun-53400.appspot.com"
-});
+})
 
 var app = express()
-var self = this;
+var self = this
 self.users = []
 self.demo = []
+self.loadSchool = []
+self.schools = []
 const usersRef = firebase.database().ref().child('users')
 const demoRef = firebase.database().ref().child('demo')
 
 //getUsers()
-getDemo()
-
-function getDemo() {
-    demoRef.on('value', function(snapshot) {
-        self.demo = snapshot.val();
-        console.log(self.demo);
-    });
-}
+loadSchool()
 
 function getUsers() {
     usersRef.on('value', function(snapshot) {
-        self.users = snapshot.val();
-        console.log(self.users);
-    });
+        self.users = snapshot.val()
+        console.log(self.users)
+    })
+}
+
+function loadSchool() {
+    firebase.database().ref().child('schools').on('child_added', function(snapshot) {
+        var item = {
+            id: snapshot.key,
+            value: snapshot.val()
+        }
+        self.schools.push(item)
+        //console.log(self.schools)
+    })
+    return self.schools
+}
+
+function loadInfoSchool(uid, req, res) {
+    self.infoSchools = []
+    firebase.database().ref('schools').child(uid).on('value', function(snapshot) {
+        var item = {
+            id: snapshot.key,
+            value: snapshot.val()
+        }
+        self.infoSchools.push(item)
+        //console.log(self.infoSchools)
+    })
+    return self.infoSchools
 }
 
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
-});
+})
 
 
-app.get('/demo', function(req, res) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-    // res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.json(self.demo)
-});
+app.get('/schools', function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With")
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST")
+    res.json(self.schools)
+})
 
-// GET method route
-app.get('/', function(req, res) {
-    res.send('GET request to the homepage')
+app.post('/infoschool/:uid/', function(req, res) {
+    console.log('ID:', req.params.uid)
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With")
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST")
+    res.json(loadInfoSchool(req.params.uid))
+    //res.json(self.infoSchools)
 })
 
 // POST method route
